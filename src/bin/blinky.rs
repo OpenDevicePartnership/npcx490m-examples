934f0a2233837380cfe76128e2bd97da16eec422
Wouter Geraedts <git@woutergeraedts.nl>
2024-12-18 16:49:09 +0100

    Added probe-rs stub to run in RAM and changed blinky LED to one that seems to work

--- src/bin/blinky.rs
+++ src/bin/blinky.rs
@@ -14,23 +14,23 @@ fn main() -> ! {
     info!("Hello world");
 
     if let Some(p) = pac::Peripherals::take() {
-        // Set pin 0 as output
-        p.gpio0.px_dir().write(|w| w.pin0().output());
+        // Set pin 7 as output
+        p.gpiob.px_dir().write(|w| w.pin7().output());
 
-        // Connect pull down on pin 0
-        p.gpio0.px_pud().write(|w| w.pin0().pull_down());
+        // Connect pull down on pin 7
+        p.gpiob.px_pud().write(|w| w.pin7().pull_down());
 
-        // Enable pull on pin 0
-        p.gpio0.px_pull().write(|w| w.pin0().enable());
+        // Enable pull on pin 7
+        p.gpiob.px_pull().write(|w| w.pin7().enabled());
 
         loop {
-            if p.gpio0.px_dout().read().pin0().is_low() {
-                p.gpio0.px_dout().write(|w| w.pin0().high());
+            if p.gpiob.px_dout().read().pin7().is_low() {
+                p.gpiob.px_dout().write(|w| w.pin7().high());
             } else {
-                p.gpio0.px_dout().write(|w| w.pin0().low());
+                p.gpiob.px_dout().write(|w| w.pin7().low());
             }
 
-            delay(500_000);
+            delay(5_000_000);
         }
     } else {
         panic!("Unable to access Peripherals");
